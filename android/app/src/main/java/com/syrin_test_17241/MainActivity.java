7a94049c7114698cde9987cfea7b049cd08cd2ee
Crowdbotics <team@crowdbotics.com>
2020-05-21 17:49:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/syrin_test_17241/MainActivity.java
@@ -0,0 +1,15 @@
+package com.syrin_test_17241;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "syrin_test_17241";
+    }
+}
